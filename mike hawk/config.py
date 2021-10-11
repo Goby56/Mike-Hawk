304feb77cbba9a0975b88171ba3dfeb1a9e3b023
Casper Bené <48201144+cappe04@users.noreply.github.com>
2021-10-11 20:08:37 +0200

    colors and screen

--- mike hawk/config.py
+++ mike hawk/config.py
@@ -5,8 +5,19 @@ BASE_FRICTION = 0.31
 PLAYER_SPEED = 10
 TERMINAL_VELOCITY = 100
 
+colors = {
+    "white": (255, 255, 255),
+    "black": (0, 0, 0),
+    "red": (255, 0, 0),
+    "blue": (0, 255, 0),
+    "green": (0, 0, 255),
+    "blackmagic": (20, 20, 20),
+    "whiteknight": (200, 200, 200)
+}
+
 import pygame, os, ctypes
 SCREENSIZE = ctypes.windll.user32.GetSystemMetrics(0), ctypes.windll.user32.GetSystemMetrics(1)
+SCREEN_WIDTH, SCREEN_HEIGHT = SCREENSIZE
 print(SCREENSIZE)
 res_dir = os.path.join("res")
 
