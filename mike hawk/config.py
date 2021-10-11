5d743cdd20cc208263520bcd4c6159d33ce9859b
Goby56 <karl.rosengren.kr@gmail.com>
2021-10-11 20:00:00 +0200

    Update config.py

--- mike hawk/config.py
+++ mike hawk/config.py
@@ -5,7 +5,9 @@ BASE_FRICTION = 0.31
 PLAYER_SPEED = 10
 TERMINAL_VELOCITY = 100
 
-import pygame, os
+import pygame, os, ctypes
+SCREENSIZE = ctypes.windll.user32.GetSystemMetrics(0), ctypes.windll.user32.GetSystemMetrics(1)
+print(SCREENSIZE)
 res_dir = os.path.join("res")
 
 spritesheet = pygame.image.load(os.path.join(res_dir, "spritesheet.png"))
