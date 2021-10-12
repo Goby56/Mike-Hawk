19b4a29e36554fba0afef924965d1c0a75af0218
Goby56 <karl.rosengren.kr@gmail.com>
2021-10-12 18:47:30 +0200

    Start to spritesheet

--- mike hawk/res/config.py
+++ mike hawk/res/config.py
@@ -29,8 +29,19 @@ PYGAME_CAPS_KEYS = {
     "space": pygame.K_SPACE
 }
 
+<<<<<<< Updated upstream
 _base_dir = os.path.abspath(os.path.dirname(__file__))
 _menu_dir = os.path.join(_base_dir, "assets", "Menu", "MenuButton.png")
 menubutton = pygame.image.load(_menu_dir)
+=======
+class Spritesheet:
+    def __init__(self, filename):
+        self.filename = filename
+        self.sheet = pygame.image.load(filename).convert()
+
+    def get_sprite()
+
+assets_dir = os.path.join("assets")
+>>>>>>> Stashed changes
 
 #spritesheet = pygame.image.load(os.path.join(assets_dir, "spritesheet.png"))
