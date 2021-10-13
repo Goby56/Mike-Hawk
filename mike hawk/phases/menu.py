3c3f080bc135a7b36d08162a388495b407e4a60b
Casper Bené <48201144+cappe04@users.noreply.github.com>
2021-10-13 20:06:05 +0200

    Map Menu

--- mike hawk/phases/menu.py
+++ mike hawk/phases/menu.py
@@ -13,7 +13,7 @@ class MainMenu(Phase):
         self.canvas, self.listener = canvas, listener
 
         self.buttonpanel = MenuButtonPanel(canvas, listener, (200, 100), 3, 20, ["New Phase", "Options", "Quit"],
-            [lambda: Game(canvas, listener).enter_phase(), lambda: OptionsMenu(canvas, listener).enter_phase(), quit])
+            [lambda: MapMenu(canvas, listener).enter_phase(), lambda: OptionsMenu(canvas, listener).enter_phase(), quit])
 
         self.counter = 0
 
@@ -25,6 +25,20 @@ class MainMenu(Phase):
         scaled_surface = pygame.transform.scale(dynamite_frames[int(self.counter%4)], (100, 100))
         rotated_surface = pygame.transform.rotate(scaled_surface, self.counter*4)
         self.canvas.blit(rotated_surface, (300, 300))
+
+
+class MapMenu(Phase):
+    def __init__(self, canvas, listener):
+        printname = lambda name: print(name)
+        self.buttonpanel = MenuButtonPanel(canvas, listener, (200, 100), 6, 20, 
+            ["Dev Map", "Map 1", "Map 2", "Map 3", "Map 4", "Map 5"],
+            [lambda: Game(canvas, listener), lambda: print("Map 1"), lambda: print("Map 2"), 
+            lambda: print("Map 3"), lambda: print("Map 4"), lambda: print("Map 5")]
+        )
+
+    def update(self, dt):
+        self.buttonpanel.update()
+
 
 
 class OptionsMenu(Phase):
