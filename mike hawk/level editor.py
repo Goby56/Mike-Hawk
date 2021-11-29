92d93558af365305fa2193fabc8a5cbad5fddfe4
Casper Bené <48201144+cappe04@users.noreply.github.com>
2021-11-29 17:00:41 +0100

    Update level editor.py

--- mike hawk/level editor.py
+++ mike hawk/level editor.py
@@ -7,7 +7,7 @@ from tkinter import messagebox as mb
 from res.tileset import load_set
 from res.widgets import MenuButtonPanel, Toolbar
 from phases.phase import Phase
-from res.config import colors, _base_dir, paralax_layers
+from res.config import colors, _base_dir, paralax_layers, editor_buttons
 from listener import Listener
 
 
@@ -25,6 +25,7 @@ def load_level(path):
     with open(path, "r") as file:
         json_dict = json.load(file)
     return json_dict
+
 
 class Menu(Phase):
     def __init__(self, canvas, listener):
@@ -103,13 +104,16 @@ class Editor(Phase):
 
         tmp = pygame.Surface((20, 20))
         tmp.fill(colors["red"])
+        pen, eraser, marker, entity, layer1, layer2 = editor_buttons
         self.toolbar = Toolbar(self.canvas, self.listener, (70, 0), 
-            [tmp, tmp, tmp, tmp, tmp], 5)
+            [pen, eraser, marker, entity, tmp], 5)
         self.toolbar.bind(("b", 0), ("e", 1), ("m", 2))
         
         self.layer_panel = Toolbar(self.canvas, self.listener, (self.toolbar.pos[0] + self.toolbar.dim[0] + 10, 0), 
-            [tmp, tmp], 5)
+            [layer1, layer2], 5)
         self.layer_panel.bind(("1", 0), ("2", 1))
+
+        self.panel = Panel(self.canvas, SCREENSIZE[0] // 8, self.tileset)
 
         self.func_keys = {"space": False, "left control": False}
         
@@ -128,7 +132,7 @@ class Editor(Phase):
         self.mode = self.modes[self.toolbar.get_selected()]
         self.layer = self.layer_panel.get_selected()
 
-        if any(self.func_keys.values()) or self.toolbar.hover() or self.layer_panel.hover(): 
+        if any(self.func_keys.values()) or any([self.toolbar.hover(), self.layer_panel.hover(), self.panel.hover()]): 
             self.mode = "other"
 
         for key in self.func_keys.keys():
@@ -137,7 +141,7 @@ class Editor(Phase):
         self.listener.on_event("quit", self.exit)
         
         self.get_movement()
-        
+
         if self.listener.mouse_clicked(1, hold=True) and self.mode in self.modes:
                 exec("self.{}()".format(self.mode))
 
@@ -146,7 +150,10 @@ class Editor(Phase):
         if self.func_keys["left control"] and self.listener.key_pressed("s"):
             self.save()
 
+
         self.draw_lines()
+        if self.mode == "place" or self.mode == "other":
+            self.panel.update(self.layer)
         self.toolbar.update()
         self.layer_panel.update()
         pygame.mouse.get_rel()
@@ -160,7 +167,7 @@ class Editor(Phase):
     def new_tile(self, x, y, index, layer):
         image = self.tileset[layer][index]
         self.tiles.append(Tile(self.canvas, (x, y), layer, image))
-        self.level["map"][f"{x}, {y}"] = [index, layer]
+        self.level["map"]["{}, {}".format(x, y)] = [index, layer]
 
     def get_movement(self):
         if self.listener.mouse_clicked(4): self.tile -= 1
@@ -174,15 +181,16 @@ class Editor(Phase):
     def place(self):
         x, y = self.mouse
         tile = self.get_tile(x, y)
-        if tile and tile.layer != self.layer:
+        index = 0
+        if tile and (tile.layer != self.layer or tile.image == self.tileset[self.layer][index]):
             return
-        self.new_tile(x, y, 0, self.layer)
+        self.new_tile(x, y, self.panel.get_selected(), self.layer)
 
     def delete(self):
         tile = self.get_tile(*self.mouse)
         if tile and tile.layer == self.layer:
             self.tiles.remove(tile)
-            del self.level["map"][f"{tile.x}, {tile.y}"]
+            del self.level["map"]["{}, {}".format(tile.x, tile.y)]
 
     def spawn(self):
         x, y = self.mouse
@@ -247,15 +255,72 @@ class Tile:
         self.x, self.y = pos
         self.image = image
         self.layer = layer
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (self.x, self.y)
 
-    def update(self, dim, x_offset, y_offset):
+    def update(self, dim, x_offset, y_offset, panel=False):
         dim = int(dim)
         surf = pygame.transform.scale(self.image, (dim,dim))
-        self.canvas.blit(surf, (self.x*dim+x_offset, SCREENSIZE[1]-(MAX_Y-self.y)*dim+y_offset))
+        self.rect = surf.get_rect()
+        self.rect.topleft = (self.x, self.y)
+
+        if panel:
+            self.canvas.blit(surf, (self.x, self.y))
+        else:    
+            self.canvas.blit(surf, (self.x*dim+x_offset, SCREENSIZE[1]-(MAX_Y-self.y)*dim+y_offset))
         if self.layer == 1:
             pygame.draw.rect(self.canvas, colors["white knight"], 
                 (self.x*dim+x_offset, SCREENSIZE[1]-(MAX_Y-self.y)*dim+y_offset, dim, dim), 2)
 
+
+
+def set_tiles(surface, images, layer, size, padding):
+    lst = []
+    i = 0
+    for y in range(len(images) // 3 + 1):
+        for x in range(3):
+            pos = (x*(size+padding) + padding, y*(size+padding) + padding)
+            lst.append(Tile(surface, pos, layer, images[i]))
+        i += 1
+    return lst[1:]
+
+
+class Panel:
+    def __init__(self, canvas, width, tileset):
+        self.canvas = canvas
+        self.surface = pygame.Surface((width, SCREENSIZE[1]))
+        self.width = width
+        self.tileset = tileset
+        self.rect = pygame.Rect(SCREENSIZE[0]-self.width, 0, self.width, SCREENSIZE[1])
+        padding = 10
+        self.size = SCREENSIZE[1] // 20
+        template = lambda i :set_tiles(self.surface, self.tileset[i], i, self.size, padding)
+        self.tiles = [template(0), template(1)]
+        self.prev_layer = 0
+        self.layer = 0
+        self.selected = self.tiles[0][0]
+
+    def hover(self):
+        mouse = pygame.mouse.get_pos()
+        if self.rect.collidepoint(*mouse): return True
+        return False
+
+    def get_selected(self):
+        return self.tiles[self.layer].index(self.selected)
+
+    def update(self, layer):
+        self.surface.fill(colors["black"])
+        self.layer = layer
+        if self.prev_layer != layer:
+            self.prev_layer = layer
+            self.selected = self.tiles[layer][0]
+        for tile in self.tiles[layer]:
+            tile.update(self.size, 0, 0, panel=True)
+            x, y = pygame.mouse.get_pos()
+            if tile.rect.collidepoint(x - self.rect.x, y - self.rect.y) and pygame.mouse.get_pressed()[0]:
+                self.selected = tile
+        pygame.draw.rect(self.surface, colors["white knight"], self.selected.rect, 3)
+        self.canvas.blit(self.surface, self.rect.topleft)
 
 if __name__ == "__main__":
     app = App()
